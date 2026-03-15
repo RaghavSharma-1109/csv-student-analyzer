97959fcde5886d9eb9a60d45f1ca80ca3dd2bb26
Raghav Sharma <raghavusharmaa@gmail.com>
2026-03-15 20:18:36 +0530

    Added student average calculation

--- analyzer.py
+++ analyzer.py
@@ -1,19 +1,35 @@
 import csv
+
+
 def load_student_data(file_path):
     students = []
+
     with open(file_path, 'r') as file:
         reader = csv.DictReader(file)
 
         for row in reader:
-            student={
+            student = {
                 "name": row["Name"],
                 "math": int(row["Math"]),
                 "physics": int(row["Physics"]),
                 "chemistry": int(row["Chemistry"])
             }
+
             students.append(student)
+
     return students
 
-data = load_student_data("data/students.csv")
-for student in data:
-    print(student) 
+
+def calculate_average(student):
+    return (student["math"] + student["physics"] + student["chemistry"]) / 3
+
+
+def main():
+    students = load_student_data("data/students.csv")
+
+    for student in students:
+        avg = calculate_average(student)
+        print(f"{student['name']} average: {avg:.2f}")
+
+
+main()
