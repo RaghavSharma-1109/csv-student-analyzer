b726df28e5c94a70b86c6e4f551b611e8487af98
Raghav Sharma <raghavusharmaa@gmail.com>
2026-04-08 19:17:20 +0530

    Fixed handler and validator class and introduced analyzer - that processes data of students

--- analyzer.py
+++ analyzer.py
@@ -1,9 +1,3 @@
-import csv
-from sqlite3.dbapi2 import PARSE_COLNAMES
-class DataTypeError(Exception):
-    pass
-class InvalidEntry(Exception):
-    pass
 class FileHandler:
     def __init__(self,filepath) -> None:
         if not filepath:
@@ -32,10 +26,11 @@ class FileHandler:
         except FileNotFoundError:
             raise FileNotFoundError('CSV file not found. Check path.')
 class DataValidator:
-    def validator(self,data):
-        self.data = []
-
-        for newdata in data:
+    def __init__(self, data) -> None:
+        self.data = data
+    def validator(self):
+        self.cleaned_data = []
+        for newdata in self.data:
             new_data = {}
             error = False
             name = newdata.get('name')
@@ -55,70 +50,15 @@ class DataValidator:
                     error = True
                     break
             if not error:
-                self.data.append(new_data)
-        return self.data
+                self.cleaned_data.append(new_data)
+        return self.cleaned_data
+file1 = FileHandler('students.csv')
+raw_data = file1.load_student_data()
+data = DataValidator(raw_data)
+cleaned_data = data.validator()
 class StudentAnalyzer:
-    def calculate_average(self,student):
-        avg = ((student["math"] + student["physics"] + student["chemistry"]) / 3)
-        return avg
-    def calculate_grade(self,avg):
-        if avg >= 90:
-            return 'A+'
-        elif avg >= 80:
-            return 'A'
-        elif avg >= 70:
-            return 'B+'
-        elif avg >= 60:
-            return 'B'
-        elif avg >= 50:
-            return 'C'
-        else:
-            return 'F'
-    def get_result(self,avg):
-        if avg>=50:
-            return 'Passed'
-        return 'Failed'
-    def get_subject_topper(self,subject):
-        return max(self.students,key=lambda x: x[subject])
-    def process_students(self):
-        for student in self.students:
-            avg = self.calculate_average(student)
-            student['avg'] = avg
-            student['grade'] = self.calculate_grade(avg)
-            student['result'] = self.get_result(avg)
-    def search_student(self):
-        name = input("Enter student name:")
-        found = False
-        for student in self.students:
-            if student['name'].lower() == name.lower():
-                print(f"Name: {name}\n")
-                print(f"SUBJECTS ->\n")
-                for subject in ['math', 'physics', 'chemistry']:
-                    print(f"-{subject} = {student[subject]}")
-                print(f"Average: {student['avg']}")
-                print(f"Grade: {student['grade']}")
-                print(f"Result: {student['result']}")
-                found = True
-        if not found:
-            print("Student Not Found!")
-    def show_topper(self):
-        topper = None
-        topper_avg = float('-inf')
-        for student in self.students:
-            if student['avg']>topper_avg:
-                topper = student
-                topper_avg = student['avg']
-        print(f"Topper: {topper['name']}")
-        print(f"Score: {topper_avg}")
-
-    def save_students_report(self,filename):
-        with open(filename, "w") as file:
-        
-            file.write(f"{'Name':<8}  {'Avg':>8}  {'Grade':<6}  {'Result':<8}\n")
-            file.write(f"-"*38)
-            for student in self.students:
-                file.write(f"\n{student['name']:<8}  {(student['avg']):>8.2f}  {student['grade']:<6}  {student['result']:<8}")
-            
+    def __init__(self, data) -> None:
+        self.cleaned_data = data
     def main(self):
         while True:
             print("\n1.Search Student")
